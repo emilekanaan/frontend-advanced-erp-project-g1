b795d9585397a54a5205560a4f63dad6672ecf06
malak-wahyb <malakwahyb537@gmail.com>
2023-03-06 19:00:11 +0200

    fixed animation

--- src/route/visitor/visitor.js
+++ src/route/visitor/visitor.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Sidebar from '../../components/sidebar/sidebar'
 import "./visitor.css"
+// import ToggleColorMode from '../../components/dark'
 
 function Visitor() {
     return (
         <div className='visitor'>
+         
             <Sidebar />
             <Outlet />
         </div>
