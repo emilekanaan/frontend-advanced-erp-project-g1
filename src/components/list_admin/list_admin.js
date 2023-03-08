9074daf574787f926d6eade4afd8cb35d235387b
IbrahimBashir123 <ibrahimbashir11111@gmail.com>
2023-03-08 13:01:33 +0200

    brahim add side

--- src/components/list_admin/list_admin.js
+++ src/components/list_admin/list_admin.js
@@ -1,10 +1,57 @@
-import React from 'react'
-import "./list_admin.css"
+import * as React from "react";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { useDemoData } from "@mui/x-data-grid-generator";
+import "./list_admin.css";
 
-function ListAdmin() {
-    return (
-        <div>ListAdmin</div>
-    )
+export default function ListAdmin() {
+  const { data } = useDemoData({
+    dataSet: "Admin", // Change the data set to "Admin"
+    rowLength: 100,
+    maxColumns: 6,
+  });
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 150 },
+    { field: "firstName", headerName: "First Name", width: 200 },
+    { field: "lastName", headerName: "Last Name", width: 200 },
+    { field: "email", headerName: "Email", width: 200 },
+    { field: "phone", headerName: "Phone", width: 150 },
+    { field: "team", headerName: "Team", width: 150 },
+    { field: "showprofile", headerName: "Show profile", width: 150 },
+    { field: "edit", headerName: "Edit", width: 150 },
+    { field: "delete", headerName: "Delete", width: 150 },
+  ];
+
+  return (
+    <>
+      <div className="admin-table">
+        <h1 style={{ color: "#f4f4f9", fontSize:"40px" }}>Admin</h1>
+        <DataGrid
+          rows={data.rows}
+          columns={columns} // Pass the columns array as a prop
+          slots={{
+            toolbar: GridToolbar,
+          }}
+          sx={{
+            marginTop: "50px",
+            boxShadow: "1px 2px 3px 3px rgba(0,0,0,0.63)",
+            border: "1px solid #16202A ",
+           
+            color: "#f4f4f9",
+
+            fontSize: "20px",
+            "& .MuiDataGrid-cell:hover": {
+              backgroundColor: "#16202A",
+              color: "#F4F4F9",
+            },
+            "& .MuiDataGrid-cell": {
+              backgroundColor:"#2f4550",
+              color: "#F4F4F9",
+              
+            },
+          }}
+        />
+      </div>
+    </>
+  );
 }
-
-export default ListAdmin
--- src/pages/employee/employee.js
+++ src/pages/employee/employee.js
@@ -3,13 +3,12 @@ import FormEmployee from "../../components/form_employee/form_employee";
 import ListEmployee from "../../components/list_employee/list_employee";
 
 function Employee() {
-    return (
-        <>
-
-           
-            <ListEmployee />
-        </>
-    );
+  return (
+    <>
+      {/* <FormEmployee /> */}
+      <ListEmployee />
+    </>
+  );
 }
 
 export default Employee;
